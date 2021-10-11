4ed8cb011cc7e6772540054cd830919a612b6dd4
Radulescu Alex <ralexandru89@yahoo.com>
2021-10-11 20:37:23 +0300

    create modal&json for add

--- functions.js
+++ functions.js
@@ -77,4 +77,21 @@ document.querySelector('#top-menu-bar').addEventListener("click", e => {
   
 })
 
+function makeModal() {
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+  btn.onclick = function() {
+    modal.style.display = "block";
+  } 
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
 
+makeModal();
